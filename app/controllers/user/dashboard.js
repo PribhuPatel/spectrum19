15b78afd170f2f100d307057b0469d109d38d5ec
Priyank Bhuva <160010116006@adit.ac.in>
2018-12-02 12:54:04 +0530

    updated user dashboard

--- app/controllers/user/dashboard.js
+++ app/controllers/user/dashboard.js
@@ -15,7 +15,7 @@ module.exports = {
         if(user===null){
         return res.send("No User Found");
     }else{
-        let today_registered = user.registered.participants.lenghth;
+        let today_registered = user.registered.participants.length;
         let today_payment = user.today_payment;
         let events = await getManyDataWithPopulate(Departments,{},'events','name','name');
         return res.json({status:true, name: user.name,today_registered: today_registered,today_payment: today_payment,eventsdata:events});

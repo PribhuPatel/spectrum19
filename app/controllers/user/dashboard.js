4b9a723cda1b92e4bbb086af20e0973f916f87e8
Priyank Bhuva <160010116006@adit.ac.in>
2019-01-02 15:57:39 +0530

    application schedule added

--- app/controllers/user/dashboard.js
+++ app/controllers/user/dashboard.js
@@ -1,6 +1,6 @@
 
 
-var {Users,Departments} = require('../../middlewares/schemas/schema');
+var {Users,Departments,Participants} = require('../../middlewares/schemas/schema');
 var {getSingleDataWithPopulate, getSingleData,getManyData, getManyDataWithPopulate, getCount} = require('../../utils/helpers/general_one_helper');
 var {verifyToken}  = require('../../middlewares/verifytoken');
 module.exports = {
@@ -15,7 +15,7 @@ module.exports = {
         if(user===null){
             return res.json({status: true,message:"No User Found"});
     }else{
-        let today_registered = await getCount({$and:[{createdby: user._id},{created_date:Date.now + 5.5}]});;
+        let today_registered = await getCount(Participants,{$and:[{createdby: user._id},{created_date:Date.now + 5.5}]});;
         let today_payment = user.today_payment;
         let events = await getManyDataWithPopulate(Departments,{},'events','name linked_department','name',{available_entries:{ $ne: 0 }});
         return res.json({status:true, today_registered: today_registered,today_payment: today_payment,eventsdata:events});

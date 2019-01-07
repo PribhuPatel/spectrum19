0f79d7500f85e0c679a3bb181e06cb353769ac70
Priyank Bhuva <160010116006@adit.ac.in>
2019-01-07 15:12:07 +0530

    event type in add event

--- app/controllers/registration/evententry/addeventpage.js
+++ app/controllers/registration/evententry/addeventpage.js
@@ -1,7 +1,7 @@
 
 
 var {Departments, Colleges,Events, Entries} = require('../../../middlewares/schemas/schema');
-var {getManyData} = require('../../../utils/helpers/general_one_helper');
+var {getManyData, getManyDataWithPopulateWithLimit} = require('../../../utils/helpers/general_one_helper');
 
 module.exports = {
   addEventPage: async (req, res) => {
@@ -17,14 +17,17 @@ module.exports = {
     },
     getLeaders: async(req,res)=>{
       let partiPhone = req.body.leader_phone;
-     let leaders =  await Entries.find({event:req.body.event_id}).populate({
-        path: 'team_leader',
-    match: {"$where": "function(){ return this.phone.toString().match(/"+partiPhone+"/)!=null;}"},
-    // Explicitly exclude `_id`, see http://bit.ly/2aEfTdB
-    select: 'phone name',
-    options: { limit: 5 }
-  }).
-  exec();
+  //    let leaders =  await Entries.find({event:req.body.event_id}).populate({
+  //       path: 'team_leader',
+  //   match: {"$where": "function(){ return this.phone.toString().match(/"+partiPhone+"/)!=null;}"},
+  //   select: 'phone name',
+  //   options: { limit: 5 }
+  // }).
+  // exec();
+  let leaders  = await  getManyDataWithPopulateWithLimit(Entries,{event:req.body.event_id},5,'team_leader','team_leader','firstname lastname phone');
+  leaders = leaders.filter(l=> l.team_leader.phone.toString().includes(partiPhone.toString()));
+  // let leaders  = await getManyDataWithPopulateWithLimit(Entries,{$and:[{event:req.body.event_id},{"$where": "function(){ return this.team_leader.phone.toString().match(/"+partiPhone+"/)!=null;}"}]},5,'phone name','team_leader')
+  // let leaders =  await Entries.find({$and:[{event:req.body.event_id},{"$where": "function(){ return this.team_leader.phone.toString().match(/"+partiPhone+"/)!=null;}"}]}).populate({
   return res.json({status:true, leaders:leaders});
     }
   };

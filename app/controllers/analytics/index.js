66237aa36a7548787f78ad8bea1ff1606097ba8b
Priyank Bhuva <160010116006@adit.ac.in>
2019-01-16 10:27:02 +0530

    added multithread

--- app/controllers/analytics/index.js
+++ app/controllers/analytics/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const {verifyToken} = require('../../middlewares/verifytoken');
 
-router.use('/admin', require('./admin'));
+router.use('/admin',verifyToken, require('./admin'));
 
 router.use('/auth', require('./auth'));
 
@@ -10,11 +10,11 @@ router.use('/coordinator', verifyToken,require('./coordinator'));
 
 router.use('/events', verifyToken,require('./events'));
 
-router.use('/add', require('./add'));
+router.use('/add',verifyToken, require('./add'));
 
-router.use('/getdata', require('./getdata'));
+router.use('/getdata',verifyToken, require('./getdata'));
 
-router.use('/csv', require('./csv'));
+router.use('/csv',verifyToken, require('./csv'));
 
 
 module.exports = router;

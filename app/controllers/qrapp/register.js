003e282cca124750679cdd1212ff0013ff313551
Priyank Bhuva <160010116006@adit.ac.in>
2019-01-31 13:03:08 +0530

    qrapp api

--- app/controllers/qrapp/register.js
+++ app/controllers/qrapp/register.js
@@ -6,12 +6,12 @@ var {getManyData,getSingleDataWithPopulate} = require('../../utils/helpers/gener
 module.exports = {
     checkQr: async (req,res)=>{
         let qr  = req.body.qr;
-        let participant = await getSingleData(Participants,{_id:qr},'college','firstname lastname phone college package','name');
+        let participant = await getSingleDataWithPopulate(Participants,{_id:qr},'college','firstname lastname phone college package','name');
         let package= false ;
         if(participant.package !=null){
             package= true;
         }
-        return res.jsun({status:true,participant:{name:participant.firstname + ' '+ participant.lastname, phone:participant.phone, college:participant.college.name}});
+        return res.json({status:true,participant:{name:participant.firstname + ' '+ participant.lastname, phone:participant.phone, college:participant.college.name,package:package}});
     }
   };
   

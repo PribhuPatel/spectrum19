c0406b3df9dc86f6f007bdadc51aabcee22fc593
Priyank Bhuva <160010116006@adit.ac.in>
2019-01-31 15:05:29 +0530

    added checks for non users

--- app/controllers/qrapp/register.js
+++ app/controllers/qrapp/register.js
@@ -2,16 +2,31 @@
 
 var {Events, Participants} = require('../../middlewares/schemas/schema');
 var {getManyData,getSingleDataWithPopulate} = require('../../utils/helpers/general_one_helper');
+const mongoose =  require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 
 module.exports = {
     checkQr: async (req,res)=>{
         let qr  = req.body.qr;
-        let participant = await getSingleDataWithPopulate(Participants,{_id:qr},'college','firstname lastname phone college package','name');
+        let participant = null
+        try{
+        let id= ObjectId(qr);
+        let parti = await getSingleDataWithPopulate(Participants,{_id:id},'college','firstname lastname phone college package','name');
+        
         let package= false ;
-        if(participant.package !=null){
+        if(parti==null || parti== undefined){
+            participant = null;
+        } else {
+            // participant = parti;
+        if(parti.package !=null){
             package= true;
         }
-        return res.json({status:true,participant:{name:participant.firstname + ' '+ participant.lastname, phone:participant.phone, college:participant.college.name,package:package}});
+            participant = {name:parti.firstname + ' '+ parti.lastname, phone:parti.phone, college:parti.college.name,package:package}
+        }
+        return res.json({status:true,participant:participant});
+    } catch(e){
+        return res.json({status:true,participant:null});
+        }
     }
   };
   

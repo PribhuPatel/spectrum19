77bbc6f9a5909101813bf0eba449b18896180f59
Priyank Bhuva <34309656+PribhuPatel@users.noreply.github.com>
2021-06-12 23:17:46 +0530

    Updating some files

--- app/utils/sendmail.js
+++ app/utils/sendmail.js
@@ -5,10 +5,8 @@ var config={
     service: 'gmail',
         auth:
         {
-        user: 'spectrum@adit.ac.in',
-        // user:'markhentony@gmail.com',
-        // pass:'markhentony@12345'
-      pass: 'Spec1700@adit2019'
+        user: 'spectrumfakr@adit.ac.in',
+      pass: 'testpass'
     }
 }
 
